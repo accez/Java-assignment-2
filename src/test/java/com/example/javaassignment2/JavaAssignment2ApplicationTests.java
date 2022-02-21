4fa7469c970719edce33da8745c6678e3bf309bf
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-02-21 13:50:57 +0100

    feat: init spring boot project

---
+++ src/test/java/com/example/javaassignment2/JavaAssignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaassignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAssignment2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
